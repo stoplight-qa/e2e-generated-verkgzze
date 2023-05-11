eb2230d14b21009aace4298c19841b04c0a67ceb
qa-oazzg-10-dmri@example.com <qa-oazzg-10-dmri@example.com>
2023-05-11 16:44:34 +0000

    First commit of promoted project

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://x-16431.stoplight-dev.com/api/v1/projects/cHJqOjE0MDU/spectral.js?branch_slug=main&token=2b605069-c2c7-4ae4-aa1a-42b92535965f";
+export default { extends: ruleset };
